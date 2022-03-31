94cce72001229c2f7dc2060a70505898c759da73
muskaanshah <hmuskaan11@gmail.com>
2022-03-31 14:23:24 +0530

    refactor: remove nested ternary

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,23 +1,24 @@
 import "./alert.css";
 
 function Alert({ type, msg }) {
+	let alertClass = "";
+	let spanClass = "info";
+
+	if (type === "success") {
+		alertClass = "alert-success";
+		spanClass = "check_circle";
+	}
+	if (type === "warning") {
+		alertClass = "alert-warning";
+		spanClass = "warning";
+	}
+	if (type === "error") {
+		alertClass = "alert-error";
+		spanClass = "error";
+	}
 	return (
-		<div
-			className={`alert borderradius-0-5 ${
-				type === "success"
-					? "alert-success"
-					: type === "warning"
-					? "alert-warning"
-					: "alert-error"
-			}`}
-		>
-			<span className="material-icons">
-				{type === "success"
-					? "check_circle"
-					: type === "warning"
-					? "warning"
-					: "error"}
-			</span>
+		<div className={`alert borderradius-0-5 ${alertClass}`}>
+			<span className="material-icons">{spanClass}</span>
 			{msg}
 		</div>
 	);
