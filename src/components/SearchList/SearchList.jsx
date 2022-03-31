2bc4824e6f73422307ec2a16df355076f03712f3
muskaanshah <hmuskaan11@gmail.com>
2022-03-31 14:13:03 +0530

    fix: searchlist closed when nothing present

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -5,12 +5,10 @@ function SearchList() {
 	const { productState } = useProduct();
 	return (
 		<div className="search-list">
-			{/* {productState.searchText !== "" && */}
-			{itemListCalculation(productState)
-				.filter((item, index) => index < 5)
-				.map((item) => (
-					<p className="search-item">{item.name}</p>
-				))}
+			{productState.searchText !== "" &&
+				itemListCalculation(productState)
+					.filter((_, index) => index < 5) // _ because item not used
+					.map((item) => <p className="search-item">{item.name}</p>)}
 		</div>
 	);
 }
