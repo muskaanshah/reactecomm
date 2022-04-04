25ecefb638aaba6cdf73576986b23ece339afecb
muskaanshah <hmuskaan11@gmail.com>
2022-04-05 03:06:31 +0530

    feat: basic coupons

--- src/pages/checkout/components/PriceCard.jsx
+++ src/pages/checkout/components/PriceCard.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useCartWishlist } from "../../../context/cart-wishlist-context";
+import { CouponsModal } from "./CouponsModal";
 
 function PriceCard() {
+	const [couponModal, setCouponModal] = useState(false);
+	const [couponDiscount, setCouponDiscount] = useState(0);
 	const { cartState } = useCartWishlist();
 	const totalActualPrice = cartState.cart.reduce(
 		(acc, curr) =>
@@ -18,19 +22,23 @@ function PriceCard() {
 		0
 	);
 	const deliveryCharge = totalActualPrice - totalDiscount > 5000 ? 0 : 99;
+	const totalPriceBeforeCoupon = totalActualPrice - totalDiscount;
 	const totalPriceAfterDiscount =
-		totalActualPrice - totalDiscount + deliveryCharge;
+		totalActualPrice - totalDiscount + deliveryCharge - couponDiscount;
 	return (
 		<div className="ordersummary price-card p-1">
-			<span className="flex-end">
-				<span className="material-icons fs-1"> local_offer </span>
-				<input
-					type="text"
-					placeholder="Enter your coupon code"
-					className="input-couponcode"
-				/>
-				<button className="btn bg-primary py-0 px-0-5 fs-0-8">Apply</button>
-			</span>
+			{couponDiscount === 0 && (
+				<span className="flex-end">
+					<span className="material-icons fs-1"> local_offer </span>
+					<span className="fw-600">Apply coupon</span>
+					<button
+						className="btn bg-primary py-0 px-0-5 fs-0-8"
+						onClick={() => setCouponModal(true)}
+					>
+						Apply
+					</button>
+				</span>
+			)}
 			<p className="my-0-5 fw-600">
 				PRICE DETAILS ({cartState.cartItemsNumber} Items)
 			</p>
@@ -43,6 +51,20 @@ function PriceCard() {
 				<p>Discount on MRP</p>
 				<p className="color-success ls-1">-₹{totalDiscount}</p>
 			</div>
+			{couponDiscount !== 0 && (
+				<div className="price-attribute">
+					<div className="price-coupon-remove">
+						<p>Coupon Discount</p>
+						<p
+							className="fs-0-8 text-underline"
+							onClick={() => setCouponDiscount(0)}
+						>
+							Remove
+						</p>
+					</div>
+					<p className="color-success ls-1">-₹{couponDiscount}</p>
+				</div>
+			)}
 			<div className="price-attribute">
 				<p>Delivery Charge</p>
 				<p>₹{deliveryCharge}</p>
@@ -55,6 +77,13 @@ function PriceCard() {
 			<Link to="/" className="btn bg-primary btn-place-order mt-1">
 				PLACE ORDER
 			</Link>
+			{couponModal && (
+				<CouponsModal
+					totalPriceBeforeCoupon={totalPriceBeforeCoupon}
+					setCouponDiscount={setCouponDiscount}
+					setCouponModal={setCouponModal}
+				/>
+			)}
 		</div>
 	);
 }
