b5b89d64cb303eeb0941044a9c9c376a89f6f188
muskaanshah <hmuskaan11@gmail.com>
2022-05-24 23:14:47 +0530

    fix: order summary

--- src/utils/RequiresAuth.js
+++ src/utils/RequiresAuth.js
@@ -3,21 +3,13 @@ import { Navigate, useLocation } from "react-router-dom";
 function RequiresAuth({ children }) {
     const location = useLocation();
     const token = localStorage.getItem("encodedToken");
-    return token ? (
-        children
-    ) : (
-        <Navigate to="/login" state={{ from: location }} replace />
-    );
+    return token ? children : <Navigate to="/login" state={{ from: location }} replace />;
 }
 
 function NotRequiresAuth({ children }) {
     const location = useLocation();
     const token = localStorage.getItem("encodedToken");
-    return token ? (
-        <Navigate to="/" state={{ from: location }} replace />
-    ) : (
-        children
-    );
+    return token ? <Navigate to="/" state={{ from: location }} replace /> : children;
 }
 
 export { RequiresAuth, NotRequiresAuth };
