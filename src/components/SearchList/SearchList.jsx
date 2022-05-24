8a1afc43a0090996a47e3dc1084770893254965f
muskaanshah <hmuskaan11@gmail.com>
2022-05-25 01:56:15 +0530

    refactor: no items found

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -7,22 +7,32 @@ function SearchList() {
     const { productState } = useProduct();
     return (
         <div className="search-list">
-            {productState.searchText !== "" &&
-                itemListCalculation(productState)
-                    .filter((_, index) => index < 5) // _ because item not used
-                    .map((item) => (
-                        <Link
-                            style={{ color: "inherit", textDecoration: "inherit" }}
-                            to={`/product/${item._id}`}
-                        >
-                            <p className="search-item">
-                                <Highlight
-                                    text={item.name}
-                                    highlight={productState.searchText}
-                                />
-                            </p>
-                        </Link>
-                    ))}
+            {productState.searchText !== "" && (
+                <>
+                    {itemListCalculation(productState).length > 0 ? (
+                        itemListCalculation(productState)
+                            .filter((_, index) => index < 5) // _ because item not used
+                            .map((item) => (
+                                <Link
+                                    style={{
+                                        color: "inherit",
+                                        textDecoration: "inherit",
+                                    }}
+                                    to={`/product/${item._id}`}
+                                >
+                                    <p className="search-item">
+                                        <Highlight
+                                            text={item.name}
+                                            highlight={productState.searchText}
+                                        />
+                                    </p>
+                                </Link>
+                            ))
+                    ) : (
+                        <div className="search-item">No items found</div>
+                    )}
+                </>
+            )}
         </div>
     );
 }
