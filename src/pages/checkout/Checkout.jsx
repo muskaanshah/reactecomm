a19ef8e4fac012856da62d4cece006119d184446
muskaanshah <hmuskaan11@gmail.com>
2022-05-26 17:37:42 +0530

    fix: small fixes

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -5,31 +5,37 @@ import { useAddress } from "../../context";
 import "./checkout.css";
 
 function Checkout() {
-	const { address } = useAddress();
-	const [selectedAddress, setSelectedAddress] = useState("");
-	return (
-		<div className="container-body py-1 px-2">
-			<div className="ordersummary-flex">
-				<div>
-					<h2 className="page-heading">Select your address</h2>
-					<div className="flex-column address-radiobox">
-						{address.map((curAddress) => (
-							<AddressCard
-								curAddress={curAddress}
-								selectedAddress={selectedAddress}
-								setSelectedAddress={setSelectedAddress}
-								key={curAddress._id}
-							/>
-						))}
-					</div>
-				</div>
-				<div>
-					<h2 className="page-heading">Order Summary</h2>
-					<PriceCard selectedAddress={selectedAddress} />
-				</div>
-			</div>
-		</div>
-	);
+  const { address } = useAddress();
+  const [selectedAddress, setSelectedAddress] = useState("");
+  return (
+    <div className="container-body py-1 px-2">
+      <div className="ordersummary-flex">
+        <div>
+          <h2 className="page-heading">Select your address</h2>
+          <div className="flex-column address-radiobox">
+            {address.length > 0 ? (
+              <>
+                {address.map((curAddress) => (
+                  <AddressCard
+                    curAddress={curAddress}
+                    selectedAddress={selectedAddress}
+                    setSelectedAddress={setSelectedAddress}
+                    key={curAddress._id}
+                  />
+                ))}
+              </>
+            ) : (
+              <h3>No addresses added yet, add an address to proceed</h3>
+            )}
+          </div>
+        </div>
+        <div>
+          <h2 className="page-heading">Order Summary</h2>
+          <PriceCard selectedAddress={selectedAddress} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export { Checkout };
--- src/pages/userProfile/components/ProfileSection.jsx
+++ src/pages/userProfile/components/ProfileSection.jsx
@@ -1,18 +1,18 @@
 import { useAuth } from "../../../context";
 
 function ProfileSection() {
-	const { user } = useAuth();
-	return (
-		<div className="profile-section px-1">
-			<h3 className="mb-0">Details</h3>
-			<p className="my-0">{`Name: ${user.firstName} ${user.lastName}`}</p>
-			<p className="my-0">Email: {user.email}</p>
-			<p className="my-0">Contact: {user.mobile}</p>
-			<span className="color-primary text-underline fw-500 fs-0-9 my-1 display-inlineblock">
+  const { user } = useAuth();
+  return (
+    <div className="profile-section px-1">
+      <h3 className="mb-0">Details</h3>
+      <p className="my-0">{`Name: ${user.firstName} ${user.lastName}`}</p>
+      <p className="my-0">Email: {user.email}</p>
+      <p className="my-0">Contact: {user.mobile}</p>
+      {/* <span className="color-primary text-underline fw-500 fs-0-9 my-1 display-inlineblock">
 				Change password
-			</span>
-		</div>
-	);
+			</span> */}
+    </div>
+  );
 }
 
 export { ProfileSection };
