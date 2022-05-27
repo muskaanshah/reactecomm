2037af2b30e16a6c7bd7b2360718dedac020d31b
muskaanshah <hmuskaan11@gmail.com>
2022-05-27 21:07:26 +0530

    feat: add address from checkout page

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -3,9 +3,11 @@ import { AddressCard } from "./components/AddressCard";
 import { PriceCard } from "./components/PriceCard";
 import { useAddress } from "../../context";
 import "./checkout.css";
+import { AddressForm } from "../userProfile/components/AddressForm";
 
 function Checkout() {
   const { address } = useAddress();
+  const [addAddressForm, setAddAddressForm] = useState(false);
   const [selectedAddress, setSelectedAddress] = useState("");
   return (
     <div className="container-body py-1 px-2">
@@ -27,12 +29,19 @@ function Checkout() {
             ) : (
               <h3>No addresses added yet, add an address to proceed</h3>
             )}
+            <label
+              className="address-selector p-0-5 centered br-4px cursor-pointer"
+              onClick={() => setAddAddressForm(true)}
+            >
+              <p className="m-0">+ Add a new address</p>
+            </label>
           </div>
         </div>
         <div>
           <h2 className="page-heading">Order Summary</h2>
           <PriceCard selectedAddress={selectedAddress} />
         </div>
+        {addAddressForm && <AddressForm setAddAddressForm={setAddAddressForm} />}
       </div>
     </div>
   );
